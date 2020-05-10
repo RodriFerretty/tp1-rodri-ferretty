5df1860816334523d1010e90cb3e1911c348e32f
RodriFerretty <rodrigoferretty@gmail.com>
2020-05-10 06:03:18 -0300

    Navigation from CountriesTable to CountryDetail with country name parameter.

--- src/app/Components/Tables/countries-table/countries-table.component.ts
+++ src/app/Components/Tables/countries-table/countries-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from 'src/app/Models/Country/country';
+import { COVIDRepositoryService } from 'src/app/APIServices/covidrepository-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-countries-table',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries-table.component.css']
 })
 export class CountriesTableComponent implements OnInit {
-
-  constructor() { }
+  countries: Country[]
+  
+  constructor(
+    private service: COVIDRepositoryService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.service.getCountriesInfo().subscribe((countries) => {
+      console.log(countries);
+      this.countries = countries;
+      console.log('VARIABLE COUNTRIES: ', this.countries);
+    });
+  }
+
+  onSelect(country: Country): void {
+    console.log('Selected country: ', country.country);
+    this.router.navigate(['/countries/', country.country ])
+    console.log("APA_: ", this.router.getCurrentNavigation())
   }
 
 }
